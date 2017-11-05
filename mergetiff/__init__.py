89a249a695b75c5a88238cb37e3dbaa16a32af83
Adam Rehn <adam.rehn@my.jcu.edu.au>
2017-11-05 18:32:02 +1000

    Improve NumPy interop

--- mergetiff/__init__.py
+++ mergetiff/__init__.py
@@ -1,1 +1,1 @@
-from .lib import *
+from .lib import openDataset, getRasterBands, getAllRasterBands, rasterFromFile, rasterToFile, rasterFromDataset, datasetFromRaster, createMergedDataset
--- mergetiff/lib.py
+++ mergetiff/lib.py
@@ -1,6 +1,47 @@
 from osgeo import gdal
 import numpy as np
 
+# Private functions
+
+def _geotiffOptions(dtype):
+	"""
+	Returns the driver options for creating a GeoTiff dataset of the specified type.
+	"""
+	
+	# Use LZW compresion with all CPU cores
+	options = []
+	options.append("NUM_THREADS=ALL_CPUS")
+	options.append("COMPRESS=LZW")
+	
+	# Use predictor=2 for integer types and predictor=3 for floating-point types
+	if (dtype == gdal.GDT_Float32 or dtype == gdal.GDT_Float64):
+		options.append("PREDICTOR=3")
+	else:
+		options.append("PREDICTOR=2")
+	
+	return options
+
+def _numpyTypeToGdalType(dtype):
+	"""
+	Returns the equivalent GDAL datatype for the specified NumPy datatype
+	"""
+	mappings = {
+		np.dtype(np.byte):    gdal.GDT_Byte,
+		np.dtype(np.int8):    gdal.GDT_Byte,
+		np.dtype(np.uint8):   gdal.GDT_Byte,
+		np.dtype(np.int16):   gdal.GDT_Int16,
+		np.dtype(np.uint16):  gdal.GDT_UInt16,
+		np.dtype(np.int32):   gdal.GDT_Int32,
+		np.dtype(np.uint32):  gdal.GDT_UInt32,
+		np.dtype(np.float32): gdal.GDT_Float32,
+		np.dtype(np.float64): gdal.GDT_Float64,
+	}
+	
+	return mappings.get(dtype, gdal.GDT_Unknown)
+
+
+# Public functions
+
 def openDataset(filename):
 	"""
 	Opens a dataset
@@ -15,11 +56,25 @@ def getRasterBands(dataset, bandIndices):
 	"""
 	return list([dataset.GetRasterBand(i) for i in bandIndices])
 
+def getAllRasterBands(dataset):
+	"""
+	Retrieves all raster bands from the supplied dataset
+	"""
+	return list([dataset.GetRasterBand(i) for i in range(1, dataset.RasterCount + 1)])
+
 def rasterFromFile(filename):
 	"""
 	Opens a dataset, reads all raster bands, and returns them as a NumPy ndarray of shape (h,w[,c])
 	"""
 	return rasterFromDataset(openDataset(filename))
+
+def rasterToFile(filename, raster):
+	"""
+	Writes a NumPy ndarray of shape (h,w[,c]) to a GeoTiff file
+	"""
+	dtype = _numpyTypeToGdalType(raster.dtype)
+	options = _geotiffOptions(dtype)
+	dataset = datasetFromRaster(raster, False, 'GTiff', filename, options)
 
 def rasterFromDataset(dataset):
 	"""
@@ -30,37 +85,30 @@ def rasterFromDataset(dataset):
 		raster = raster.transpose((1, 2, 0))
 	return raster
 
-def datasetFromRaster(raster, forceGrayInterp = False):
+def datasetFromRaster(raster, forceGrayInterp = False, driver = 'MEM', filename='', options=[]):
 	"""
-	Takes a NumPy ndarray of shape (h,w[,c]) and returns an in-memory GDAL Dataset
+	Takes a NumPy ndarray of shape (h,w[,c]) and creates and returns a GDAL Dataset.
+	
+	If forceGrayInterp is False and the image has more than three channels, the first
+	four channels will have R, G, B, and Alpha colour interpretation, respectively.
+	
+	By default, an in-memory dataset is created using the GDAL MEM driver.
 	"""
 	
 	# If the raster image is only single-channel, add the extra dimension to its shape
 	if len(raster.shape) == 2:
-		raster = raster[:,np.newaxis]
-	
-	# Mappings from NumPy datatypes to their equivalent GDAL datatypes
-	typeMappings = {
-		np.dtype(np.byte):    gdal.GDT_Byte,
-		np.dtype(np.int8):    gdal.GDT_Byte,
-		np.dtype(np.uint8):   gdal.GDT_Byte,
-		np.dtype(np.int16):   gdal.GDT_Int16,
-		np.dtype(np.uint16):  gdal.GDT_UInt16,
-		np.dtype(np.int32):   gdal.GDT_Int32,
-		np.dtype(np.uint32):  gdal.GDT_UInt32,
-		np.dtype(np.float32): gdal.GDT_Float32,
-		np.dtype(np.float64): gdal.GDT_Float64,
-	}
+		raster = raster[:,:,np.newaxis]
 	
 	# Create an in-memory dataset
 	numBands = raster.shape[2]
-	driver = gdal.GetDriverByName('MEM')
+	driver = gdal.GetDriverByName(driver)
 	dataset = driver.Create(
-		'',
+		filename,
 		raster.shape[1],
 		raster.shape[0],
 		numBands,
-		typeMappings[raster.dtype]
+		_numpyTypeToGdalType(raster.dtype),
+		options
 	)
 	
 	# Copy the raster bands
@@ -87,19 +135,11 @@ def datasetFromRaster(raster, forceGrayInterp = False):
 def createMergedDataset(filename, metadataDataset, rasterBands):
 	"""
 	Creates a new dataset with all of the metadata from the specified input dataset,
-	and with each of the raster bands in the supplied list
+	and with each of the raster bands in the supplied list.
+	
+	List items can be either gdal.Band instances or 2D NumPy arrays. In the case of
+	NumPy arrays, a grayscale colour interpretation will be applied.
 	"""
-	
-	# Use LZW compresion with all CPU cores
-	options = []
-	options.append("NUM_THREADS=ALL_CPUS")
-	options.append("COMPRESS=LZW")
-	
-	# Use predictor=2 for integer types and predictor=3 for floating-point types
-	if (rasterBands[0].DataType == gdal.GDT_Float32 or rasterBands[0].DataType == gdal.GDT_Float64):
-		options.append("PREDICTOR=3")
-	else:
-		options.append("PREDICTOR=2")
 	
 	# Create the output dataset
 	driver = gdal.GetDriverByName('GTiff')
@@ -109,23 +149,32 @@ def createMergedDataset(filename, metadataDataset, rasterBands):
 		rasterBands[0].YSize,
 		len(rasterBands),
 		rasterBands[0].DataType,
-		options
+		_geotiffOptions(rasterBands[0].DataType)
 	)
 	
 	# Copy the metadata from the input dataset
-	dataset.SetGeoTransform( metadataDataset.GetGeoTransform() )
-	dataset.SetProjection( metadataDataset.GetProjection() )
-	dataset.SetMetadata( metadataDataset.GetMetadata() )
+	if metadataDataset != None:
+		dataset.SetGeoTransform( metadataDataset.GetGeoTransform() )
+		dataset.SetProjection( metadataDataset.GetProjection() )
+		dataset.SetMetadata( metadataDataset.GetMetadata() )
 	
 	# Copy the GCPs from the input dataset, if it has any
-	if metadataDataset.GetGCPCount() > 0:
+	if metadataDataset != None and metadataDataset.GetGCPCount() > 0:
 		dataset.SetGCPs( metadataDataset.GetGCPs(), metadataDataset.GetGCPProjection() )
 	
 	# Copy each of the input raster bands
 	for index, inputBand in enumerate(rasterBands):
 		
-		# Copy the raster data
+		# Retrieve the output band
 		outputBand = dataset.GetRasterBand(index+1)
+		
+		# If the input band is a 2D NumPy array, copy the raster data and interpret it as grayscale
+		if not hasattr(inputBand, 'ReadAsArray'):
+			outputBand.WriteArray(inputBand)
+			outputBand.SetColorInterpretation(gdal.GCI_GrayIndex)
+			continue
+		
+		# Copy the band data
 		outputBand.WriteArray( inputBand.ReadAsArray() )
 		
 		# Copy the "no data" sentinel value, if any
