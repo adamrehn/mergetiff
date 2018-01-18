73d4117c11aa3b9beb805ba50468b21a5f63ec32
Adam Rehn <adam.rehn@my.jcu.edu.au>
2018-01-18 19:15:00 +1000

    Factor out memory-wrap filename generation into a separate function

--- mergetiff/lib.py
+++ mergetiff/lib.py
@@ -58,6 +58,35 @@ def _gdalTypeToNumpyType(dtype):
 	
 	return mappings.get(dtype, np.dtype(float))
 
+def _memWrapFilename(raster):
+	"""
+	Generates the GDAL filename to create an in-memory dataset that wraps a NumPy array.
+	"""
+	
+	# Verify that the NumPy's memory is contiguous
+	if raster.flags['C_CONTIGUOUS'] != True:
+		raise RuntimeError('NumPy array must be row-major and contiguous')
+	
+	# Retrieve the underlying pointer for the NumPy array
+	pointer, read_only_flag = raster.__array_interface__['data']
+	
+	# Retrieve the image dimensions
+	cols  = raster.shape[0]
+	rows  = raster.shape[1]
+	bands = raster.shape[2] if len(raster.shape) > 2 else 1
+	
+	# Build the filename for the memory-wrap dataset
+	return 'MEM:::DATAPOINTER={},PIXELS={},LINES={},BANDS={},DATATYPE={},PIXELOFFSET={},LINEOFFSET={},BANDOFFSET={}'.format(
+		hex(pointer),
+		cols,
+		rows,
+		bands,
+		gdal.GetDataTypeName(_numpyTypeToGdalType(raster.dtype)),
+		bands * raster.dtype.itemsize,
+		cols * bands * raster.dtype.itemsize,
+		raster.dtype.itemsize
+	)
+
 
 # Public functions
 
@@ -155,33 +184,7 @@ def wrapRasterData(raster):
 	"""
 	Wraps a NumPy array in a GDAL in-memory dataset. The memory is only wrapped, no copy is made.
 	"""
-	
-	# Verify that the NumPy's memory is contiguous
-	if raster.flags['C_CONTIGUOUS'] != True:
-		raise RuntimeError('NumPy array must be row-major and contiguous')
-	
-	# Retrieve the underlying pointer for the NumPy array
-	pointer, read_only_flag = raster.__array_interface__['data']
-	
-	# Retrieve the image dimensions
-	cols  = raster.shape[0]
-	rows  = raster.shape[1]
-	bands = raster.shape[2] if len(raster.shape) > 2 else 1
-	
-	# Build the filename for the memory-wrap dataset
-	filename = 'MEM:::DATAPOINTER={},PIXELS={},LINES={},BANDS={},DATATYPE={},PIXELOFFSET={},LINEOFFSET={},BANDOFFSET={}'.format(
-		hex(pointer),
-		cols,
-		rows,
-		bands,
-		gdal.GetDataTypeName(_numpyTypeToGdalType(raster.dtype)),
-		bands * raster.dtype.itemsize,
-		cols * bands * raster.dtype.itemsize,
-		raster.dtype.itemsize
-	)
-	
-	# Attempt to create the memory-wrap dataset
-	return gdal.Open(filename, gdal.GA_ReadOnly)
+	return gdal.Open(_memWrapFilename(raster), gdal.GA_ReadOnly)
 
 def createMergedDataset(filename, metadataDataset, rasterBands, progressCallback=None):
 	"""
